89e2a35777adc74f3eb8590de8069d0510193938
matthewkilleen0830 <matthew.killeen0830@gmail.com>
2021-03-11 13:50:28 -0600

    Daily backup.  Queries file and ERD complete.

---
+++ queries.sql
@@ -0,0 +1,73 @@
+-- Query #1:  join employees table with salaries table and query for
+-- and list employee number, last name, first name, sex, and salary
+select employees.emp_no as "Employee #",
+employees.last_name as "Last Name",
+employees.first_name as "First Name",
+employees.sex as "Gender",
+salaries.salary as "Annual Salary"
+from employees
+inner join salaries on
+employees.emp_no = salaries.emp_no;
+
+-- Query #2:  query for first name, last name, and hire date for employees hired in 1986
+select first_name as "First Name",
+last_name as "Last Name",
+hire_date as "Date of Hire"
+from employees where hire_date >= '1986-01-01 00:00:00' and hire_date < '1987-01-01 00:00:00';
+
+-- Query #3:  join departments, department manager, and employees tables and query for manager of each department
+-- and list department number, department name, employee number, last name, and first name
+select departments.dept_no as "Department #",
+departments.dept_name as "Department Name",
+dept_manager.emp_no as "Employee #",
+employees.last_name as "Last Name",
+employees.first_name as "First Name"
+from dept_manager
+inner join departments on dept_manager.dept_no = departments.dept_no
+inner join employees on dept_manager.emp_no = employees.emp_no
+order by departments.dept_no;
+
+-- Query #4:  join departments, department employees, and employees tables and query for department of each employee
+-- and list employee number, last name, first name, and department name
+select departments.dept_name as "Department Name",
+dept_emp.emp_no as "Employee #",
+employees.last_name as "Last Name",
+employees.first_name as "First Name"
+from dept_emp
+inner join departments on dept_emp.dept_no = departments.dept_no
+inner join employees on dept_emp.emp_no = employees.emp_no;
+
+-- Query #5:  query for first name, last name, and sex for employees named "Hercules" with last names beginning with "B"
+select first_name as "First Name",
+last_name as "Last Name",
+sex as "Gender"
+from employees where first_name = 'Hercules' and last_name like 'B%';
+
+-- Query #6:  join departments, department employees, and employees tables and query for all employees in the sales department
+-- and list employee number, last name, first name, and department name
+select departments.dept_name as "Department Name",
+dept_emp.emp_no as "Employee #",
+employees.last_name as "Last Name",
+employees.first_name as "First Name"
+from departments
+inner join dept_emp on dept_emp.dept_no = departments.dept_no
+inner join employees on dept_emp.emp_no = employees.emp_no
+where departments.dept_name = 'Sales';
+
+-- Query #7:  join departments, department employees, and employees tables and query for all employees in the sales
+-- and the development departments, and list employee number, last name, first name, and department name
+select departments.dept_name as "Department Name",
+dept_emp.emp_no as "Employee #",
+employees.last_name as "Last Name",
+employees.first_name as "First Name"
+from departments
+inner join dept_emp on dept_emp.dept_no = departments.dept_no
+inner join employees on dept_emp.emp_no = employees.emp_no
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
+
+-- Query #8:  query for the count of unique employee last names and list in descending order
+select count(last_name) as "Number of People",
+last_name as "Employee Last Name"
+from employees
+group by last_name
+order by "Number of People" desc;
